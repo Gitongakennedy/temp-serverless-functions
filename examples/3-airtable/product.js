b9393670afc384b2a458e09e1e0c6444c6474abe
Gitongakennedy <kennygitongac@gmail.com>
2023-05-08 18:56:47 +0300

    continous deployment

---
+++ examples/3-airtable/product.js
@@ -0,0 +1,25 @@
+const result=document.querySelector('.result')
+
+const fetchData= async ()=>{
+ console.log('Single Product page')
+try {
+ const id=window.location.search 
+ const {data:{fields}}= await axios.get(`/api/3-product${id}`)
+ const {name,image,price,description}=fields
+ return `<h1 class="title">Single Product</h1>
+  <article class="product">
+    <img class="product-img"
+    src=${image}
+    alt=${name}
+    />
+    <div class="product-info">
+      <h5 class="title">${name}</h5>
+      <h5 class="price">$${price}</h5>
+      <p class="desc">${description}</p>
+    </div>
+  </article>`
+} catch (error) {
+ result.innerHTML=`<h2>${error.response.data}</h2>`
+}
+}
+fetchData()
